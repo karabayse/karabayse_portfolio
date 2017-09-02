2d8da884d93ef6cd70fa8bc9f02b5de7211a960d
Kara Bayse <karabayse@gmail.com>
2017-09-02 15:42:17 -0500

    contact and image changes

--- modules/routes/index.js
+++ modules/routes/index.js
@@ -1,4 +1,14 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "",
+        pass: ""
+    }
+});
+
 var path = require('path');
 var router = express.Router();
 var bodyParser = require('body-parser');
@@ -6,6 +16,7 @@ var bodyParser = require('body-parser');
 // uses
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
+// router.use(nodemailer.static('public'));
 // router.use(function(req, res) {
 //     res.sendfile(__dirname + 'public/views/index.html');
 // });
@@ -15,5 +26,23 @@ router.get('/*', function(req, res) {
   res.sendFile(path.resolve('public/views/index.html'));
 }); // end router.get for index.html
 
+// nodemailer get call
+router.get('/send', function(req, res) {
+  var mailOptions={
+        to : req.query.to,
+        subject : req.query.subject,
+        text : req.query.text
+    };
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var express = require('express');
 var app = express();
 var index = require('./modules/routes/index');
 var portfolio = require('./modules/routes/portfolio');
+var nodemailer = require('nodemailer');
 
 // uses
 app.use(express.static('public'));
 app.use('/', index);
 app.use('/porfolio', portfolio);
+// app.use(nodemailer.static('public'));
 
 // globals
 var port = process.env.PORT || 6378;
