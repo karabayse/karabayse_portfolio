9c7f0399ea57b66f0f353990c8d969c4f7968150
Kara Bayse <karabayse@gmail.com>
2017-10-05 17:19:57 -0500

    module fixes

--- modules/routes/email.js
+++ modules/routes/email.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var userModel = require('../userModel');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
--- modules/routes/portfolio.js
+++ modules/routes/portfolio.js
@@ -3,11 +3,6 @@ var path = require('path');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-
-var userModel = require('../userModel');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
 // uses
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
