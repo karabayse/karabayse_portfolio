e4454275232c66f1ac7e498b2ad08b46f4c27551
Kara Bayse <karabayse@gmail.com>
2017-10-05 16:08:56 -0500

    add service code

--- public/scripts/services/portfolioService.js
+++ public/scripts/services/portfolioService.js
@@ -1,7 +1,18 @@
+/*---- Portfolio Service ----*/
 
+myApp.service('PortfolioService', function($http) {
 
-$http({
-  method: 'POST',
-  data: emailObject,
-  url: '/portfolio'
-});
+  var sv = this;
+
+  // POST call for submitEmail
+  sv.submitEmail = function(email) {
+    return $http({
+      method: 'POST',
+      url: '/portfolio',
+      data: emailObject
+    }).then(function(response){
+      console.log('back from submitEmail post:', response);
+    });
+  }; // end postLocation
+
+}); // end myApp.service
